2d1501067577fac50e12d0902b6e39c67404f961
Ludmuterol <ludmuterol@yahoo.com>
2022-02-12 04:50:37 +0100

    moved conv to load.c, 1 array for each component

--- load.c
+++ load.c
@@ -1,32 +1,36 @@
 #include <windows.h>
 #include <stdio.h>
 
-typedef struct {
-        BYTE    Y;
-        BYTE    Cb;
-        BYTE    Cr;
+typedef struct
+{
+    BYTE Y;
+    BYTE Cb;
+    BYTE Cr;
 } YCbCrTRIPLE;
 
-typedef struct {
-        BITMAPFILEHEADER bf;
-        BITMAPINFOHEADER bi;
-        RGBTRIPLE* imageData;
+typedef struct
+{
+    BITMAPFILEHEADER bf;
+    BITMAPINFOHEADER bi;
+    RGBTRIPLE *imageData;
 } BMPIMAGE;
-void load(BMPIMAGE *image, char *path){
+
+void load(BMPIMAGE *image, char *path)
+{
     //open bmp file
     FILE *in = fopen(path, "rb");
 
     //reading fileheader
     //BITMAPFILEHEADER bf;
-    fread(&(image->bf), sizeof (BITMAPFILEHEADER), 1, in);
-    
+    fread(&(image->bf), sizeof(BITMAPFILEHEADER), 1, in);
+
     //reading infoheader
     //BITMAPINFOHEADER bi;
-    fread(&(image->bi), sizeof (BITMAPINFOHEADER), 1, in);
- 
+    fread(&(image->bi), sizeof(BITMAPINFOHEADER), 1, in);
+
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0. If not, quit
     if (image->bf.bfType != 0x4d42 || image->bf.bfOffBits != 54 || image->bi.biSize != 40 ||
-                    image->bi.biBitCount != 24 || image->bi.biCompression != 0)
+        image->bi.biBitCount != 24 || image->bi.biCompression != 0)
     {
         fclose(in);
         printf("Unsupported file format.\n");
@@ -42,4 +46,31 @@ void load(BMPIMAGE *image, char *path){
 
     //closing inputfile since no longer needed
     fclose(in);
+}
+
+YCbCrTRIPLE *convert(BMPIMAGE *image)
+{
+    YCbCrTRIPLE *convImage;
+
+    //allocating memory for converted image
+    convImage = (YCbCrTRIPLE *)malloc(image->bi.biSizeImage);
+
+    //each pixel gets converted from RGB to YCbCr
+    for (int i = 0; i < image->bi.biHeight; i++)
+    {
+        for (int j = 0; j < image->bi.biWidth; j++)
+        {
+            //current position on converted image
+            int convPos = i * image->bi.biWidth + j;
+
+            //current position on bmp image
+            int origPos = (image->bi.biHeight - 1 - i) * image->bi.biWidth + j;
+
+            //conversion table from: https://www.w3.org/Graphics/JPEG/jfif3.pdf (26.01.22)
+            convImage[convPos].Y  =  0.299  * image->imageData[origPos].rgbtRed + 0.5876 * image->imageData[origPos].rgbtGreen + 0.114  * image->imageData[origPos].rgbtBlue;
+            convImage[convPos].Cb = -0.1687 * image->imageData[origPos].rgbtRed - 0.3313 * image->imageData[origPos].rgbtGreen + 0.5    * image->imageData[origPos].rgbtBlue + 128;
+            convImage[convPos].Cr =  0.5    * image->imageData[origPos].rgbtRed - 0.4187 * image->imageData[origPos].rgbtGreen - 0.0813 * image->imageData[origPos].rgbtBlue + 128;
+        }
+    }
+    return (convImage);
 }
--- main.c
+++ main.c
@@ -2,56 +2,65 @@
 #include <stdlib.h>
 #include <windows.h>
 #include "load.c"
-YCbCrTRIPLE * convert(BMPIMAGE *image){
-    YCbCrTRIPLE* convImage;
-
-    //allocating memory for converted image
-    convImage = (YCbCrTRIPLE *)malloc(image->bi.biSizeImage);
-
-    //each pixel gets converted from RGB to YCbCr
-    for (int i = 0; i < image->bi.biHeight; i++)
-    {
-        for (int j = 0; j < image->bi.biWidth; j++)
-        {
-            //current position on converted image
-            int convPos = i * image->bi.biWidth + j;
-
-            //current position on bmp image
-            int origPos = (image->bi.biHeight - 1 - i) * image->bi.biWidth + j;
-
-            //conversion table from: https://www.w3.org/Graphics/JPEG/jfif3.pdf (26.01.22)
-            convImage[convPos].Y =    0.299   * image->imageData[origPos].rgbtRed + 0.5876 * image->imageData[origPos].rgbtGreen + 0.114  * image->imageData[origPos].rgbtBlue;
-            convImage[convPos].Cb = - 0.1687  * image->imageData[origPos].rgbtRed - 0.3313 * image->imageData[origPos].rgbtGreen + 0.5    * image->imageData[origPos].rgbtBlue + 128;
-            convImage[convPos].Cr =   0.5     * image->imageData[origPos].rgbtRed - 0.4187 * image->imageData[origPos].rgbtGreen - 0.0813 * image->imageData[origPos].rgbtBlue + 128;
-        
-        }
-    }
-    return(convImage);
-}
 
 void main()
 {
+    //load image
     BMPIMAGE image;
-    load(&image,"parrot.bmp");
+    load(&image, "parrot.bmp");
+
+    //convert image from RGB to YCbCr (convImage has no infos anymore, just image data)
+    YCbCrTRIPLE *convImage = convert(&image);
+
+    //free imagedata since not needed anymore
+    free(image.imageData);
+
+    //allocate memory for each component
+    BYTE *YImage  = (BYTE *)malloc(image.bi.biSizeImage / 3);
+    BYTE *CbImage = (BYTE *)malloc(image.bi.biSizeImage / 3);
+    BYTE *CrImage = (BYTE *)malloc(image.bi.biSizeImage / 3);
+
+    for (int i = 0; i < image.bi.biHeight; i++)
+    {
+        for (int j = 0; j < image.bi.biWidth; j++)
+        {
+            //current position in array
+            int curPos = i * image.bi.biWidth + j;
+
+            //split into three separate arrays
+            YImage[curPos]  = convImage[curPos].Y;
+            CbImage[curPos] = convImage[curPos].Cb;
+            CrImage[curPos] = convImage[curPos].Cr;
+        }
+    }
     
-    YCbCrTRIPLE* convImage = convert(&image);
+    //free convImage, since not needed anymore
+    free(convImage);
 
     //create output file
     FILE *out = fopen("out.bmp", "wb");
 
     //write fileheader
-    fwrite(&(image.bf), sizeof (BITMAPFILEHEADER), 1, out);
+    fwrite(&(image.bf), sizeof(BITMAPFILEHEADER), 1, out);
 
     //write infoheader
-    fwrite(&(image.bi), sizeof (BITMAPINFOHEADER), 1, out);
+    fwrite(&(image.bi), sizeof(BITMAPINFOHEADER), 1, out);
 
     //write pixels
-    fwrite(convImage, image.bi.biSizeImage, 1, out);
-    
+    //fwrite(convImage, image.bi.biSizeImage, 1, out); //to use this line instead of for loop move free from above to below
+    for (int i = 0; i<image.bi.biSizeImage / 3;i++)
+    {
+        fwrite(YImage  + i,1, 1, out);
+        fwrite(CbImage + i,1, 1, out);
+        fwrite(CrImage + i,1, 1, out);
+    }
+
     //close and free stuff
     fclose(out);
-    free(image.imageData);
-    free(convImage);
+    free(YImage);
+    free(CbImage);
+    free(CrImage);
+
     //quick way to see if ran to the end
     printf("successful\n");
 }
